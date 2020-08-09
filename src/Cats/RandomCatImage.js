1130c08308bf170a19cb1100b0ef7ec35723a648
Ibrakhim Ilyassov <ibrakhim@stripe.com>
2020-08-09 12:59:40 +0100

    HW2: Step 1

--- src/Cats/RandomCatImage.js
+++ src/Cats/RandomCatImage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import CountDown from 'ant-design-pro/lib/CountDown';
 
 const MainContainer = styled.div`
   display: flex;
@@ -10,10 +11,21 @@ const Image = styled.img`
   width: 100%;
 `;
 
+const TextContainer = styled.div`
+  size: 15;
+  margin-left: auto;
+  margin-right: auto;
+
+  margin-bottom: 15px;
+`;
+
 // https://docs.thecatapi.com/
 
+
 function RandomCatImage(props) {
+
   const [catImageUrl, setCatImageUrl] = React.useState(null);
+  var targetTime = new Date().getTime() + 31000;
 
   React.useEffect(() => {
     fetch("https://api.thecatapi.com/v1/images/search")
@@ -27,8 +39,13 @@ function RandomCatImage(props) {
 
   if (catImageUrl == null) return <div> Loading </div>;
 
+
   return (
     <MainContainer>
+      <TextContainer> 
+        New cat image will appear in <CountDown target={targetTime} onEnd={() => { window.location.reload(false); }} > </CountDown> seconds 
+      </TextContainer>
+      
       <Image src={catImageUrl} />
     </MainContainer>
   );
