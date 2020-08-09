12d472974bdc381d5f2426a99e136c085ddd81ec
Ibrakhim Ilyassov <ibrakhim@stripe.com>
2020-08-09 11:33:15 +0100

    Homework 1

--- src/Cats/RandomCatFact.js
+++ src/Cats/RandomCatFact.js
@@ -1,9 +1,40 @@
 import React from "react";
+import styled from 'styled-components'
 
-//https://alexwohlbruck.github.io/cat-facts/docs/
+const CAT_FACT_API = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1'
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextContainer = styled.div`
+  width: 100%;
+`;
 
 function RandomCatFact() {
-  return;
+  const [catFactText, setCatFactText] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(CAT_FACT_API)
+      .then((response) => {
+        return response.json();
+      }) 
+      .then((data) => {
+        console.log(data);
+        setCatFactText(data.text);
+      });
+  }, []);
+
+
+  if(catFactText == null)
+    return <div> Loading ... </div>;
+
+  return (
+    <MainContainer>
+      <TextContainer> {catFactText} </TextContainer>
+    </MainContainer>
+  );
 }
 
 export default RandomCatFact;
--- src/Dogs/RandomDogImage.js
+++ src/Dogs/RandomDogImage.js
@@ -1,1 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
 // https://dog.ceo/dog-api/
+
+const DOG_API = 'https://dog.ceo/api/breeds/image/random'
+
+function RandomDogImage(props) {
+  const [dogImageUrl, setDogImageUrl] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(DOG_API)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        setDogImageUrl(data.message);
+      });
+
+  }, []);
+
+  if (dogImageUrl == null) return <div> Loading .... </div>
+
+  return (
+      <MainContainer>
+          <Image src={dogImageUrl} />
+      </MainContainer>
+  )
+
+
+}
+
+export default RandomDogImage;
